4c1adedcd1f6384ebe76ef88d2fd4bc0b0c75996
007 <dr007cc@gmail.com>
2022-10-27 10:16:04 +0330

    refactor: clean code

--- config.h
+++ config.h
@@ -1,24 +1,24 @@
 
 static const char *fonts[] = { "monospace:size=20", "emoji:size=18" };
 
-static const char *tabs[8] = {"😀", "🐴", "🍌", "🎮", "🗻", "💡", "☯", "F"};
+static const char *tabs[8] = {"😀", "🐴", "🍌", "🎮", "🗻", "💡", "☯", "⭐"};
 
-static const char *e_people[] = {"☺"};
-static const char *e_nature[] = {"0","0","0","0","0"};
-static const char *e_foods[] = {"1"};
-static const char *e_activity[] = {"2"};
-static const char *e_places[] = {"3"};
-static const char *e_object[] = {"4"};
-static const char *e_symbol[] = {"5"};
-static const char *e_favorites[] = {"5"};
+static const char *e_people[] = {"X"};
+static const char *e_nature[] = {"X"};
+static const char *e_foods[] = {"X"};
+static const char *e_activity[] = {"X"};
+static const char *e_places[] = {"X"};
+static const char *e_object[] = {"X"};
+static const char *e_symbol[] = {"X"};
+static const char *e_favorites[] = {"X"};
 
 static EmojiSet emojis[8] = {
-    { e_people, 1 },
-    { e_nature, 5   },
-    { e_foods, 1    },
-    { e_activity, 1 },
-    { e_places, 1   },
-    { e_object, 1   },
-    { e_symbol, 1   },
-    { e_favorites, 1   },
+    { e_people,     1   },
+    { e_nature,     1   },
+    { e_foods,      1   },
+    { e_activity,   1   },
+    { e_places,     1   },
+    { e_object,     1   },
+    { e_symbol,     1   },
+    { e_favorites,  1   },
 };
